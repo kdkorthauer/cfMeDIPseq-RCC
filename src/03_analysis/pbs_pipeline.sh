2d82d567338051ad554017fbd7e8c761b1ea2537
kdkorthauer <kdkorthauer@gmail.com>
2020-01-24 17:49:52 -0800

    convert slurm to pbs

---
+++ src/03_analysis/pbs_pipeline.sh
@@ -0,0 +1,31 @@
+# Main slurm controller workflow
+
+# initial MEDIPS run
+WINDOWSIZE=300
+export WINDOWSIZE
+ntop=300
+export ntop
+summary=0
+export summary
+echo summary
+qsub pbs_run.sh
+
+
+# iterate DMR detection
+iter=0
+export iter
+qsub slurm_one_iteration.sh
+
+for iter in {1..73}; do
+  export iter
+  qsub slurm_one_iteration.sh
+  sleep 0.5
+done
+
+# summarize and make figures
+summary=1
+echo summary
+export summary
+qsub slurm_run.sh
+
+
---
+++ src/03_analysis/pbs_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -l walltime=8:00:00,select=1:ncpus=1:mem=60gb
+#PBS -N medip
+#PBS -V
+#PBS -A st-kdkortha-1
+#PBS -o /scratch/st-kdkortha-1/_pbs/cfMeDIPseq/JAN2020_medip.out
+#PBS -e /scratch/st-kdkortha-1/_pbs/cfMeDIPseq/JAN2020_medip.err
+
+echo $summary
+
+if [ $summary -eq 0 ]; then
+  # Run this first
+  R CMD BATCH --quiet --no-restore --no-save /arc/project/st-kdkortha-1/cfMeDIPseq-RCC/src/03_analysis/20200120_MEDIPS_RCC.R /scratch/st-kdkortha-1/_pbs/cfMeDIPseq/20200120_MEDIPS_RCC.Rout
+  R CMD BATCH --quiet --no-restore --no-save /arc/project/st-kdkortha-1/cfMeDIPseq-RCC/src/03_analysis/20200120_DMRs_RCC.R /scratch/st-kdkortha-1/_pbs/cfMeDIPseq//20200120_DMRs_RCC.Rout
+else
+  # Summarize
+  R CMD BATCH --quiet --no-restore --no-save /arc/project/st-kdkortha-1/cfMeDIPseq-RCC/src/03_analysis20190730_RCC_figures_manuscript.R /scratch/st-kdkortha-1/_pbs/cfMeDIPseq/20190730_RCC_figures_manuscript.Rout
+fi
